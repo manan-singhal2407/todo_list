1dce59b12f9dbd319dd75edad0e878d4d507015c
Manan Singhal <singhal.manan24@gmail.com>
2023-07-12 17:52:41 +0530

    Fetch initial data from API

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,23 @@
-var todoList = [];
+let todoList = [];
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => {
+            todoList.push({ taskName: item["title"], id: item["id"] });
+        });
+        renderList();
+    })
+    .catch(error => {
+        console.error('Error: ', error);
+    });
 
 function addTaskToList() {
     const taskInputField = document.getElementById("task_input_field")
     const taskName = taskInputField.value.trim();
     if (taskName !== "") {
         const id = todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1;
-        todoList.push({taskName, id});
+        todoList.push({ taskName: taskName, id: id });
         taskInputField.value = "";
         renderList();
     }
@@ -22,7 +34,7 @@ function renderList() {
 
     todoList.forEach(task => {
         const li = document.createElement("li");
-        li.innerHTML = '${task.taskName} <img src="assets/delete.png" alt="Delete Task" onclick="removeTaskFromListWithId(${task.id})">';
+        li.innerHTML = task.taskName + '<img src="assets/delete.png" alt="Delete Task" onClick=removeTaskFromListWithId(' + task.id + ')>';
         taskListElement.appendChild(li);
     });
 }
